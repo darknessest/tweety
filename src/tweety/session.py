4488b160032d78938821212b3d6c344c985bf989
Darknessest <32790500+darknessest@users.noreply.github.com>
2023-11-23 09:39:11 -0600

    accessing `user` with `.get()`

--- src/tweety/session.py
+++ src/tweety/session.py
@@ -21,7 +21,7 @@ class Session:
             with open(self.session_file_path, "r") as f:
                 session_data = json.load(f)
                 self.cookies = session_data['cookies']
-                self.user = session_data['user']
+                self.user = session_data.get('user')
 
             self.logged_in = True
 
